02e87a1ac0eaa3a060f4a59391334380068f1b1b
Yao Wang <yaowang@microsoft.com>
2023-07-27 16:36:35 +0800

    Update Write-Text.ps1

--- WriteText/Write-Text.ps1
+++ WriteText/Write-Text.ps1
@@ -1,5 +1,8 @@
 param(
     [Parameter()]
+    [int]$IntParam,
+
+    [Parameter()]
     [double]$DoubleParam,
 
     [Parameter()]
@@ -12,6 +15,7 @@ param(
     [array]$ArrayParam
 )
 
+Write-Output "IntParam value is $IntParam"
 Write-Output "DoubleParam value is $DoubleParam"
 Write-Output "StringParam value is $StringParam"
 Write-Output "BoolParam value is $BoolParam"
